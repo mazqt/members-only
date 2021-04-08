b4801230d10f0a2869f8a08a19d149c9d741a1c2
mazqt <jesperhipersson@gmail.com>
2021-04-09 01:39:24 +0200

    Fixing for deployment

--- db/migrate/20210408000320_create_posts.rb
+++ db/migrate/20210408000320_create_posts.rb
@@ -3,9 +3,16 @@ class CreatePosts < ActiveRecord::Migration[6.1]
     create_table :posts do |t|
       t.string :title
       t.text :content
-      t.references :user, null: false, foreign_key: true
+      #Development
+      #t.references :user, null: false, foreign_key: true
+      #Deployment
+      t.references :user, index: true
 
       t.timestamps
     end
+
+    #For Deployment
+    add_foreign_key :posts, :users, column: :author_id
+
   end
 end
